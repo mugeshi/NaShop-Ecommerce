175ef7f3931851eaae841b5c24a4a1a0a9480303
mugeshi <nicolemugeshi@gmail.com>
2024-06-30 00:27:07 +0300

    Setup Node.js with express and dotenv

--- api/index.js
+++ api/index.js
@@ -1,1 +1,18 @@
-console.log('hi')
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv")
+dotenv.config();
+
+
+//test route
+app.get("/", (req, res)=>{
+ res.send("app is running");
+});
+
+
+const PORT = process.env.PORT;
+app.listen(PORT || 9000 ,() =>{
+    console.log(`server listening on port ${PORT}`)
+
+});
+    
